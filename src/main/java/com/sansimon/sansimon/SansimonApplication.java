80729eb7f9bbbb4fd45f8a7b8c47562518a26e44
Calvin Cattaneo <calvin.cattaneo@gmail.com>
2018-08-28 11:20:03 -0300

    Projeto Criado

---
+++ src/main/java/com/sansimon/sansimon/SansimonApplication.java
@@ -0,0 +1,12 @@
+package com.sansimon.sansimon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SansimonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SansimonApplication.class, args);
+	}
+}
